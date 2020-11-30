06dd14954c354c285106787b40808fb077426322
Anna Madej <aniamadej123@gmail.com>
2020-11-30 18:16:37 +0100

    added LocaleChangeInterceptor

--- src/main/java/com/aniamadej/sublokator/config/config.java
+++ src/main/java/com/aniamadej/sublokator/config/config.java
@@ -1,9 +1,24 @@
 package com.aniamadej.sublokator.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.aniamadej.sublokator")
-public class config {
+public class config implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
 }
